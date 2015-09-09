7c08fd14a9d9eef3d3527dd5c049b621d41f7a9b
cogorou <cogorou@gmail.com>
2015-09-09 18:28:43 +0900

    modify

--- build/TestPlayer/MainForm.designer.cs
+++ build/TestPlayer/MainForm.designer.cs
@@ -37,23 +37,25 @@
 			this.toolPause = new System.Windows.Forms.ToolStripButton();
 			this.toolStop = new System.Windows.Forms.ToolStripButton();
 			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolReset = new System.Windows.Forms.ToolStripButton();
 			this.statusbar = new System.Windows.Forms.StatusStrip();
 			this.statusFrameIndex = new System.Windows.Forms.ToolStripStatusLabel();
 			this.statusSeeking = new System.Windows.Forms.ToolStripStatusLabel();
 			this.statusSpacer = new System.Windows.Forms.ToolStripStatusLabel();
 			this.statusImageSize = new System.Windows.Forms.ToolStripStatusLabel();
 			this.panelView = new System.Windows.Forms.Panel();
+			this.pictureView = new System.Windows.Forms.PictureBox();
 			this.timerUpdateUI = new System.Windows.Forms.Timer(this.components);
 			this.trackStartPosition = new System.Windows.Forms.TrackBar();
 			this.trackStopPosition = new System.Windows.Forms.TrackBar();
-			this.pictureView = new System.Windows.Forms.PictureBox();
-			this.toolReset = new System.Windows.Forms.ToolStripButton();
+			this.trackCurrentPosition = new System.Windows.Forms.TrackBar();
 			this.toolbar.SuspendLayout();
 			this.statusbar.SuspendLayout();
 			this.panelView.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureView)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.trackStartPosition)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.trackStopPosition)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureView)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackCurrentPosition)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// toolbar
@@ -131,14 +133,25 @@
 			this.toolStripSeparator2.Name = "toolStripSeparator2";
 			this.toolStripSeparator2.Size = new System.Drawing.Size(6, 35);
 			// 
+			// toolReset
+			// 
+			this.toolReset.AutoSize = false;
+			this.toolReset.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolReset.Image = ((System.Drawing.Image)(resources.GetObject("toolReset.Image")));
+			this.toolReset.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolReset.Name = "toolReset";
+			this.toolReset.Size = new System.Drawing.Size(32, 32);
+			this.toolReset.Text = "Reset";
+			this.toolReset.Click += new System.EventHandler(this.toolReset_Click);
+			// 
 			// statusbar
 			// 
 			this.statusbar.AutoSize = false;
 			this.statusbar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusImageSize,
             this.statusFrameIndex,
-            this.statusSeeking,
             this.statusSpacer,
-            this.statusImageSize});
+            this.statusSeeking});
 			this.statusbar.Location = new System.Drawing.Point(0, 496);
 			this.statusbar.Name = "statusbar";
 			this.statusbar.Size = new System.Drawing.Size(903, 24);
@@ -148,25 +161,25 @@
 			// statusFrameIndex
 			// 
 			this.statusFrameIndex.Name = "statusFrameIndex";
-			this.statusFrameIndex.Size = new System.Drawing.Size(15, 18);
+			this.statusFrameIndex.Size = new System.Drawing.Size(15, 19);
 			this.statusFrameIndex.Text = "x";
 			// 
 			// statusSeeking
 			// 
 			this.statusSeeking.Name = "statusSeeking";
-			this.statusSeeking.Size = new System.Drawing.Size(15, 18);
+			this.statusSeeking.Size = new System.Drawing.Size(15, 19);
 			this.statusSeeking.Text = "x";
 			// 
 			// statusSpacer
 			// 
 			this.statusSpacer.Name = "statusSpacer";
-			this.statusSpacer.Size = new System.Drawing.Size(843, 18);
+			this.statusSpacer.Size = new System.Drawing.Size(843, 19);
 			this.statusSpacer.Spring = true;
 			// 
 			// statusImageSize
 			// 
 			this.statusImageSize.Name = "statusImageSize";
-			this.statusImageSize.Size = new System.Drawing.Size(15, 18);
+			this.statusImageSize.Size = new System.Drawing.Size(15, 19);
 			this.statusImageSize.Text = "x";
 			// 
 			// panelView
@@ -175,8 +188,18 @@
 			this.panelView.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.panelView.Location = new System.Drawing.Point(0, 35);
 			this.panelView.Name = "panelView";
-			this.panelView.Size = new System.Drawing.Size(903, 397);
+			this.panelView.Size = new System.Drawing.Size(903, 365);
 			this.panelView.TabIndex = 2;
+			// 
+			// pictureView
+			// 
+			this.pictureView.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.pictureView.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureView.Location = new System.Drawing.Point(0, 0);
+			this.pictureView.Name = "pictureView";
+			this.pictureView.Size = new System.Drawing.Size(903, 365);
+			this.pictureView.TabIndex = 0;
+			this.pictureView.TabStop = false;
 			// 
 			// timerUpdateUI
 			// 
@@ -208,49 +231,43 @@
 			this.trackStopPosition.TickFrequency = 10;
 			this.trackStopPosition.Scroll += new System.EventHandler(this.trackStopPosition_Scroll);
 			// 
-			// pictureView
+			// trackCurrentPosition
 			// 
-			this.pictureView.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-			this.pictureView.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.pictureView.Location = new System.Drawing.Point(0, 0);
-			this.pictureView.Name = "pictureView";
-			this.pictureView.Size = new System.Drawing.Size(903, 397);
-			this.pictureView.TabIndex = 0;
-			this.pictureView.TabStop = false;
-			// 
-			// toolReset
-			// 
-			this.toolReset.AutoSize = false;
-			this.toolReset.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.toolReset.Image = ((System.Drawing.Image)(resources.GetObject("toolReset.Image")));
-			this.toolReset.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolReset.Name = "toolReset";
-			this.toolReset.Size = new System.Drawing.Size(32, 32);
-			this.toolReset.Text = "Reset";
-			this.toolReset.Click += new System.EventHandler(this.toolReset_Click);
+			this.trackCurrentPosition.AutoSize = false;
+			this.trackCurrentPosition.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.trackCurrentPosition.Enabled = false;
+			this.trackCurrentPosition.Location = new System.Drawing.Point(0, 400);
+			this.trackCurrentPosition.Name = "trackCurrentPosition";
+			this.trackCurrentPosition.Size = new System.Drawing.Size(903, 32);
+			this.trackCurrentPosition.TabIndex = 4;
 			// 
 			// MainForm
 			// 
+			this.AllowDrop = true;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(903, 520);
 			this.Controls.Add(this.panelView);
+			this.Controls.Add(this.trackCurrentPosition);
 			this.Controls.Add(this.trackStartPosition);
 			this.Controls.Add(this.trackStopPosition);
 			this.Controls.Add(this.statusbar);
 			this.Controls.Add(this.toolbar);
 			this.Name = "MainForm";
 			this.Text = "demo";
-			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Demo2Form_FormClosing);
-			this.Load += new System.EventHandler(this.Demo2Form_Load);
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
 			this.toolbar.ResumeLayout(false);
 			this.toolbar.PerformLayout();
 			this.statusbar.ResumeLayout(false);
 			this.statusbar.PerformLayout();
 			this.panelView.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureView)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.trackStartPosition)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.trackStopPosition)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureView)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackCurrentPosition)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -276,6 +293,7 @@
 		private System.Windows.Forms.TrackBar trackStopPosition;
 		private System.Windows.Forms.PictureBox pictureView;
 		private System.Windows.Forms.ToolStripButton toolReset;
+		private System.Windows.Forms.TrackBar trackCurrentPosition;
 	}
 }
 
